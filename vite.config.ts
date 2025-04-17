541fcfb5e465b38953fd8e284b19d04a25bee530
Your Name <your.email@example.com>
2025-04-16 23:22:06 -0500

    Update vite config and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sheep1_new/',  // 确保这个值与仓库名称匹配
+  base: './',  // 修改为相对路径
   plugins: [
     vue(),
     Unocss(),
